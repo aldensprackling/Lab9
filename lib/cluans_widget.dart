1b67d8e19e070efbb4de11dfb672b45421a054d8
Alden Sprackling <aldensprackling@Aldens-Air.lan>
2024-04-18 17:39:03 -0500

    deleted cockroachdb reference and body of cluans_widget.dart

--- lib/cluans_widget.dart
+++ lib/cluans_widget.dart
@@ -1,4 +1,3 @@
-import 'cockroachdb.dart';
 import 'package:intl/intl.dart';
 import 'package:postgres/postgres.dart';
 import 'package:flutter/material.dart';
@@ -16,30 +15,6 @@ class CluansWidget extends StatelessWidget {
     final DateFormat dateFormatter = DateFormat('yyyy-dd-MM');
 
     //TODO: make changes
-    return Scaffold(
-      appBar: AppBar(title: const Text('Hello, CockroachDB')),
-      body: FutureBuilder(
-        future: fetchCluans(),
-        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-          if (!snapshot.hasData) {
-            return const Text('Waiting for data');
-          } else {
-            Result results =
-                snapshot.data!; // a Result is an "ListView<ResultRow>"
-            for (ResultRow row in results) {
-              print(row
-                  .toColumnMap()); // this turns each row, a ListView<dynamic>, into a map
-            }
-            return ListView.builder(
-                itemCount: results.length,
-                itemBuilder: (context, index) => ListTile(
-                    title: Text(results[index][2].toString()),
-                    subtitle: Text(
-                      results[index][3].toString(),
-                    )));
-          }
-        },
-      ),
-    );
+    return Scaffold();
   }
 }
