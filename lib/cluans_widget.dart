d3b80ad6bf876afb171b7249b344e4f7ccbe9c79
Alden Sprackling <aldensprackling@Aldens-Air.lan>
2024-04-18 17:45:06 -0500

    added cluans to cluans_widget.dart

--- lib/cluans_widget.dart
+++ lib/cluans_widget.dart
@@ -1,9 +1,11 @@
 import 'package:intl/intl.dart';
-import 'package:postgres/postgres.dart';
+import 'package:provider/provider.dart';
+import 'cluans_model.dart';
 import 'package:flutter/material.dart';
 
 /// A widget that displays a list of Cluans and provides sorting options.
 class CluansWidget extends StatelessWidget {
+
   /// Constructs a new [CluansWidget] instance.
   ///
   /// The [model] parameter specifies the CluansModel containing the list of Cluans.
@@ -14,7 +16,57 @@ class CluansWidget extends StatelessWidget {
     // Formatter for displaying dates
     final DateFormat dateFormatter = DateFormat('yyyy-dd-MM');
 
-    //TODO: make changes
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Cluans',
+          style: TextStyle(
+            fontWeight: FontWeight.bold, color: Colors.white
+          )
+        ),
+      ),
+      body: Consumer<CluansModel>(
+        builder: (context, model, child) {
+          return Column(
+            children: [
+              Expanded(
+                child: ListView.separated(
+                  itemCount: model.cluans.length,
+                  itemBuilder: (context, index) {
+                    String answer = model.cluans[index].answer;
+                    String clue = model.cluans[index].clue;
+                    String date = dateFormatter.format(model.cluans[index].date);
+          
+                    return ListTile(
+                      title: Text(answer),
+                      subtitle: Text(clue),
+                      trailing: Text(date),
+                    );
+                  },
+                  separatorBuilder: (context, index) => const Divider(
+                    color: Colors.blue, 
+                    thickness: 1.0, height: 1.0
+                  )
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: () { model.sortByClue(); },
+                    child: const Text('Sort by Clue'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () { model.sortByAnswer(); },
+                    child: const Text('Sort by Answer'),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 40),
+            ],
+          );
+        },
+      )
+    );
   }
 }
